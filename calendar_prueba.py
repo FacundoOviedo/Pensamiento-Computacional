9cf2515e984ee6574e04a3e6e75a0d1639c148a9
FacundoOviedo <facundooviedo96@gmail.com>
2022-11-14 09:01:29 -0300

    Calculadora
    Primer Calculadora

---
+++ calendar_prueba.py
@@ -0,0 +1,7 @@
+import calendar
+
+c = calendar.LocaleTextCalendar(
+    calendar.SUNDAY)
+
+print(calendar.prmonth (2022, 11))
+
---
+++ index.py
@@ -0,0 +1,43 @@
+#VariableTipo
+Tipo = "Totalmente Sintetico" , 	
+"Castrol Edge" , 	
+"Castrol Edge Extented Perfomance" , 		
+"Castrol Magnatec Diesel 5W-402" , 		
+"Castrol Magnatec Start-Stop" , 		
+"Semi-Sinteticos" , 		
+"Castrol GTX UltraClean" , 	
+"Castrol Magnatec" , 		
+"Convencional" , 		
+"Castrol GTX UltraClean" , 	
+"Castrol GTX Diesel" 
+
+#VariablesCantidad
+Cantidad = range(1000)
+
+
+#VariableZona
+Zona = 1, 2 , 3 , 4
+
+#VariableCliente
+Cliente = "Import"
+
+#VariableFechaDeEntrega
+FechadeEntrega = "Calendar"
+
+while True:
+        tipo = input('ingrese tipo')
+        if tipo != "Totalmente Sintetico""Castrol Edge""Castrol Edge Extented Perfomance""Castrol Magnatec Diesel 5W-402""Castrol Magnatec Start-Stop""Semi-Sinteticos""Castrol GTX UltraClean""Castrol Magnatec""Convencional""Castrol GTX UltraClean""Castrol GTX Diesel":
+            print("Pone un tipo valido")
+        else:
+            print("Genial")
+            break
+        continue
+
+while True:
+        cantidad = int(input('ingrese cantidad'))
+        if cantidad != range(1000):
+            print("elija una cantidad valida")
+        else:
+            print("Bien")
+            break
+        continue
